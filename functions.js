23a3bd83636bf1386a6783bdf8aebbf83fe10664
Mariia <yanashama@gmail.com>
2020-06-18 12:56:19 +0300

    дз6

--- functions.js
+++ functions.js
@@ -1,5 +1,19 @@
-
+// это первое
 let test = function anonimous() {
     return "I am function expression"
 }
 anonimous();
+
+// это второе
+function mch1(num) {
+    return num * 5;
+}
+function mch2(num) {
+    return num * 10;
+}
+function multiplyChain(num) {
+    let five = mch1(num);
+    let ten = mch2(five);
+    return ten;
+}
+console.log(multiplyChain(3));
